f0fdc2117d7981b74e0915bb868b2fd301bb55b3
NareshYadav@45 <chinthalanareshyadav@gmail.com>
2024-03-15 19:19:44 +0530

    String s variable added

--- src/main/java/com/mavne/quickstart/Standalone_Maven.java
+++ src/main/java/com/mavne/quickstart/Standalone_Maven.java
@@ -6,6 +6,7 @@ public class Standalone_Maven {
 		System.out.println(5/2);
                                    System.out.println("new line added");
                                        System.out.println("another new line added");
+									   String s="abc";
 
 
                                      
